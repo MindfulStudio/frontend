588dd08002ba19f8d29d5eba9f7e500e5c94bbd7
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-09-01 17:18:12 +0200

    typo/setup typo theme in tailwind config, added css variables, created text components

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,39 @@
+import PageTitle from "@/components/typo/PageTitle";
+import HighlightText from "@/components/typo/HighlightText";
+import UserFeedbackText from "@/components/typo/UserFeedbackText";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div>
+      {/* re-usable text component: pass the title of the corresponding page as a prop*/}
+      <PageTitle pagetitle="ErrorPage" />
 
-export default ErrorPage
+      {/* directly styled element: */}
+      <h2 className="border-b pt-2 text-lg font-primary tracking-tight">
+        404 - Page not found
+      </h2>
+
+      {/* If you don´t put any font family etc. here, the default is Cantarell */}
+      <p>Everything will be fine.</p>
+
+      <p className="font-display pb-5">Just go back to Home.</p>
+
+      <p>Here we test some font style components:</p>
+
+      {/* re-usable highlight text component: pass the text and the font size as props */}
+      <HighlightText highlighttext="I´m a big Highlight!" fontsize="text-lg" />
+      <HighlightText
+        highlighttext="I´m a medium Highlight!"
+        fontsize="text-md"
+      />
+
+      {/* re-usable feedback text component: pass the feedback text and the type of feedback as props */}
+      <UserFeedbackText content="This is a feedback." type="error" />
+      <UserFeedbackText content="This is a feedback." type="validation" />
+      <UserFeedbackText content="This is a feedback." type="info" />
+      <UserFeedbackText content="This is a feedback." type="tooltip" />
+    </div>
+  );
+};
+
+export default ErrorPage;
