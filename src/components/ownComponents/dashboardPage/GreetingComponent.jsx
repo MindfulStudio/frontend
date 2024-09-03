8921dcabb22e3d9d931ff9a822a9809c9002d19a
luisePkt <madame.moon@gmx.de>
2024-09-03 19:21:26 +0200

    component/popover : adjusting popover

--- src/components/ownComponents/dashboardPage/GreetingComponent.jsx
+++ src/components/ownComponents/dashboardPage/GreetingComponent.jsx
@@ -1,19 +1,22 @@
 import { useEmotionsContext } from "@/utils/EmotionsProvider";
 import { useState } from "react";
+import { PopoverDots } from "@/components/usedDemoComponents/Popover";
 
 const GreetingComponent = () => {
   // username aus fetch von backend:
   // Gefühle der bisherigen check-ins aus dem backend:
   //   const { userData } = useEmotionsContext();
   const usernameFake = "Luise";
-  const fakeFeelings = ["müde", "munter"];
+  const fakeFeelings = ["fit", "munter", "gereizt", "aufgeregt"];
 
   // state für check-ins an diesem Tag:
   //   const [checkedIn, setCheckedIn] = useState(false);
   const [checkedIn, setCheckedIn] = useState(true);
 
   // TODO: Verbindung Provider
-  //TODO: Fetching in Provider und States einrichten
+  // TODO: Fetching in Provider und States einrichten
+  // TODO: Logik für ... bzw popover -> soll nur angezeigt werden, wenn mehr als zwei Gefühle eingecheckt wurden
+  //   TODO: popover weite und position
 
   return (
     <div className="w-[293px]">
@@ -26,10 +29,26 @@ const GreetingComponent = () => {
           {/* wann Umbrüche? */}
           <p>
             Heute hast du dich bisher{" "}
-            <span className="font-bold">{fakeFeelings[0]}</span>,{" "}
-            <span className="font-bold">{fakeFeelings[1]}</span>, ... gefühlt.
+            {/* <span className="font-bold">{fakeFeelings[0]}</span>,{" "}
+            <span className="font-bold">{fakeFeelings[1]}</span>,{" "} */}
+            <span>
+              {fakeFeelings.slice(0, 2).map((feeling, index) => {
+                return (
+                  <p key={feeling} className="inline-block pr-1">
+                    {feeling},
+                  </p>
+                );
+              })}
+            </span>{" "}
+            <PopoverDots
+              title={`...`}
+              descriptionPopover={`Du hast heute bisher folgende Gefühle erfasst:`}
+              textPopover={`${fakeFeelings}`}
+            />{" "}
+            gefühlt.
           </p>
           {/* TODO: fakeFellings noch richtig einbinden und ... brauchen noch funktionalität um alle Gefühle anzuzeigen */}
+          {/* ... soll nur angezeigt werde, wenn mehr als zwei feelings in der Liste sind -> Logik dafür einbinden */}
           <p className="font-bold mt-5">Was fühlst du gerade?</p>
         </div>
       ) : (
