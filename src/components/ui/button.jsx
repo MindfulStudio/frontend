0b339d8df29ba325021b32a6bc478a288a1a3457
luisePkt <madame.moon@gmx.de>
2024-09-04 18:44:02 +0200

    page/home/register/login : setup HomePage.jsx

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -21,12 +21,17 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
         extend:
           "bg-accent text-accent-foreground hover:bg-primary hover:text-primary-foreground ",
+        homeWhite:
+          "bg-accent-foreground text-accent hover:text-accent-foreground hover:bg-accent text-[14px] font-primary font-bold",
+        homeBlack:
+          "bg-accent text-accent-foreground hover:bg-primary hover:text-primary-foreground text-[14px] font-primary font-bold",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        home: "h-[40px] w-[123px]",
       },
     },
     defaultVariants: {
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,33 @@
+import HighlightText from "@/components/typo/HighlightText";
+import { Button } from "@/components/ui/button";
+
+// TODO: hintergrundfarbe schwart und nur oberen kasten in rosa
+// TODO: design beenden
+// TODO: register und login einrichten
+// TODO: NAV muss ausgeblendet werden
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <main className="pt-[109px] px-[50px] flex flex-col items-center">
+      {/* login */}
+      <div className="h-[386px] w-[282px] flex flex-col items-center ">
+        <HighlightText highlighttext={"Welcome"} fontsize="text-lg" />
+        <p className="text-sm pt-3 pb-6">
+          Diese App soll ein Werkzeug sein, um die eigenen Gedanken und Gefühle
+          genauer zu verstehen und die eigenen emotionalen Reaktionen besser zu
+          bewältigen. Mit der Zeit können Sie so ein tieferes Verständnis für
+          sich selbst entwickeln.
+        </p>
+        <Button size={"home"} variant={"homeWhite"}>
+          Einloggen
+        </Button>
+      </div>
+      {/* register */}
+      <div className="w-[282px]  flex flex-col items-center ">
+        <p>Neu hier?</p>
+      </div>
+    </main>
+  );
 };
 
 export default HomePage;
