019cd4128bd176cd856d2a59b2def16b0ea98844
luisePkt <madame.moon@gmx.de>
2024-09-05 23:37:35 +0200

    button active if checkob is checked

--- src/components/usedDemoComponents/Checkbox.jsx
+++ src/components/usedDemoComponents/Checkbox.jsx
@@ -2,14 +2,23 @@
 
 import { Checkbox } from "@/components/ui/checkbox";
 import { RegisterPopover } from "./RegisterPopover";
+import { useState } from "react";
 
-export function CheckboxTherms() {
+export function CheckboxTherms({ ontoggle }) {
   // AGB aus json => TODO: json: AGB
   const agb = "Es gibt noch keine AGB.";
 
+  const [acceptAGB, setAcceptAGB] = useState(false);
+
+  const handleAcceptAGB = () => {
+    setAcceptAGB(!acceptAGB);
+    ontoggle(acceptAGB);
+  };
+  console.log(acceptAGB);
+
   return (
     <div className="flex items-center space-x-2">
-      <Checkbox id="terms" />
+      <Checkbox id="terms" checked={acceptAGB} onClick={handleAcceptAGB} />
       <label
         htmlFor="terms"
         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
--- src/components/usedDemoComponents/RegisterTabs.jsx
+++ src/components/usedDemoComponents/RegisterTabs.jsx
@@ -10,8 +10,15 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent } from "@/components/ui/tabs";
 import { CheckboxTherms } from "./Checkbox";
+import { useEffect, useState } from "react";
 
 export function RegisterTabs() {
+  // activate button after checkox ist checked:
+  const [activeButton, setActiveButton] = useState(false);
+
+  const activateButton = () => {
+    setActiveButton(!activeButton);
+  };
 
   return (
     <Tabs defaultValue="account" className="w-[400px]">
@@ -33,10 +40,11 @@ export function RegisterTabs() {
               <Label htmlFor="password">Passwort</Label>
               <Input id="password" placeholder="Passwort" />
             </div>
-            <CheckboxTherms />
+            <CheckboxTherms ontoggle={activateButton} />
           </CardContent>
           <CardFooter>
-            <Button>Registrieren</Button>
+            {/* {activeButton && <Button>Registrieren</Button>} */}
+            <Button disabled={!activeButton}>Registrieren</Button>
           </CardFooter>
         </Card>
       </TabsContent>
