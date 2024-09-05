cd83693e00f667ad202b03f27769b0a973e6c8dc
luisePkt <madame.moon@gmx.de>
2024-09-05 22:58:49 +0200

    pages/login/register : adding popover to checkbox

--- src/components/usedDemoComponents/Checkbox.jsx
+++ src/components/usedDemoComponents/Checkbox.jsx
@@ -1,8 +1,12 @@
 // "use client"
 
-import { Checkbox } from "@/components/ui/checkbox"
+import { Checkbox } from "@/components/ui/checkbox";
+import { RegisterPopover } from "./RegisterPopover";
 
 export function CheckboxTherms() {
+  // AGB aus json => TODO: json: AGB
+  const agb = "Es gibt noch keine AGB.";
+
   return (
     <div className="flex items-center space-x-2">
       <Checkbox id="terms" />
@@ -10,8 +14,8 @@ export function CheckboxTherms() {
         htmlFor="terms"
         className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
       >
-        Accept terms and conditions
+        <RegisterPopover title={"AGB akzeptieren"} textPopover={agb} />
       </label>
     </div>
-  )
+  );
 }
