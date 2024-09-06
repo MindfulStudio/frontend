009a082727c3bc9f775a939efad10a26ec4977c9
luisePkt <147039751+luisePkt@users.noreply.github.com>
2024-09-06 09:48:03 +0200

    Page/config page (#69)
    * setup: folder feelingsFamilies
    
    * page/configPage : setup ConfigPage
    
    * page/configPage : adding description on ConfigPage

---
+++ src/components/usedDemoComponents/ConfigSwitch.jsx
@@ -0,0 +1,11 @@
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+
+export function ConfigSwitch({ id, name }) {
+  return (
+    <div className="flex items-center space-x-2">
+      <Switch id={id} />
+      <Label htmlFor={id}>{name}</Label>
+    </div>
+  );
+}
