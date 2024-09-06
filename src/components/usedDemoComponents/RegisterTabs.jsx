8a6d4ab7984a3780dd74ada57cec0b0fbc0fb1c6
luisePkt <madame.moon@gmx.de>
2024-09-06 11:48:37 +0200

    /register : starting implementing logic

--- src/components/usedDemoComponents/RegisterTabs.jsx
+++ src/components/usedDemoComponents/RegisterTabs.jsx
@@ -10,42 +10,85 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent } from "@/components/ui/tabs";
 import { CheckboxTherms } from "./Checkbox";
+import EyeOpenIcon from "/src/assets/icons/eye-svgrepo-com.svg";
+import EyeClosedIcon from "/src/assets/icons/eye-close-svgrepo-com.svg";
 import { useState } from "react";
+
+// TODO: captcha
+// TODO: frontend validation
 
 export function RegisterTabs() {
   // activate button after checkox ist checked:
   const [activeButton, setActiveButton] = useState(false);
+  // show/hide password
+  const [showPassword, setShowPassword] = useState(false);
 
+  // activate button after checkox ist checked:
   const activateButton = () => {
     setActiveButton(!activeButton);
   };
+
+  // show/hide password
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  // submit Input - register POST
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const registerDataToSend = new FormData();
+    registerDataToSend.append("email", registerDataToSend.email);
+    registerDataToSend.append("username", registerDataToSend.username);
+    registerDataToSend.append("password", registerDataToSend.password);
+  };
+  console.log("userdata", registerDataToSend);
 
   return (
     <Tabs defaultValue="account" className="w-[350px]">
       <TabsContent value="account">
         <Card>
-          <CardHeader>
-            <CardTitle>Registrieren</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" placeholder="Email" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" placeholder="Username" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="password">Passwort</Label>
-              <Input id="password" placeholder="Passwort" />
-            </div>
-            <CheckboxTherms ontoggle={activateButton} />
-          </CardContent>
-          <CardFooter>
-            {/* {activeButton && <Button>Registrieren</Button>} */}
-            <Button disabled={!activeButton}>Registrieren</Button>
-          </CardFooter>
+          <form onSubmit={handleSubmit}>
+            <CardHeader>
+              <CardTitle>Registrieren</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" placeholder="Email" type="email" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="username">Username</Label>
+                <Input id="username" placeholder="Username" type="text" />
+              </div>
+              <div className="space-y-1 relative">
+                <Label htmlFor="password">Passwort</Label>
+                <Input
+                  id="password"
+                  placeholder="Passwort"
+                  type={showPassword ? "text" : "password"}
+                />
+                {showPassword ? (
+                  <EyeOpenIcon
+                    className="w-5 h-auto absolute right-4 top-9"
+                    onClick={handleShowPassword}
+                  />
+                ) : (
+                  <EyeClosedIcon
+                    className="w-5 h-auto absolute right-4 top-9"
+                    onClick={handleShowPassword}
+                  />
+                )}
+              </div>
+              <CheckboxTherms ontoggle={activateButton} />
+            </CardContent>
+            <CardFooter>
+              {/* {activeButton && <Button>Registrieren</Button>} */}
+              <Button disabled={!activeButton} type="submit">
+                Registrieren
+              </Button>
+            </CardFooter>
+          </form>
         </Card>
       </TabsContent>
     </Tabs>
