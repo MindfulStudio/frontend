1ba34bb27af941a22919f67ad0a89994e90a745a
luisePkt <madame.moon@gmx.de>
2024-09-10 15:18:33 +0200

    component/navMenu : adding profile pic to navMenu

--- src/components/usedDemoComponents/ConfigSwitch.jsx
+++ src/components/usedDemoComponents/ConfigSwitch.jsx
@@ -1,25 +1,10 @@
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
-// import { useState } from "react";
 
-export function ConfigSwitch({ id, name, isActive, ontoggle }) {
-  // const [toggleActive, setToggleActive] = useState(false);
-
-  const handleToggle = () => {
-    // setToggleActive(!toggleActive);
-    // ontoggle(toggleActive);
-    ontoggle(!isActive);
-  };
-
+export function ConfigSwitch({ id, name }) {
   return (
     <div className="flex items-center space-x-2">
-      <Switch
-        id={id}
-        onClick={handleToggle}
-        // checked={toggleActive}
-        checked={isActive}
-        state={isActive ? "checked" : "unchecked"}
-      />
+      <Switch id={id} onClick={handleToggle} />
       <Label htmlFor={id}>{name}</Label>
     </div>
   );
--- src/pages/UserDataPage.jsx
+++ src/pages/UserDataPage.jsx
@@ -1,11 +1,5 @@
-import { UserDataTabs } from "@/components/usedDemoComponents/UserDataTabs";
-
 const UserDataPage = () => {
-  return (
-    <main className="flex flex-col items-center w-full h-screen pt-[109px]">
-      <UserDataTabs/>
-    </main>
-  );
+  return <main>UserDataPage</main>;
 };
 
 export default UserDataPage;
