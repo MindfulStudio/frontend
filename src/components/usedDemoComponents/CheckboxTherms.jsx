6f401d8341b2ebbadbbb976e97f7da898f5d7ee6
Baris Balci <barisbalcimusic@gmail.com>
2024-09-11 11:45:53 +0200

    feature/registration: ADD: server-side error handlings, countdown & redirect. UPDATE: password & username validation rules, some refactoring, some UI improvements.

--- src/components/usedDemoComponents/CheckboxTherms.jsx
+++ src/components/usedDemoComponents/CheckboxTherms.jsx
@@ -4,7 +4,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { RegisterPopover } from "./RegisterPopover";
 import { useState } from "react";
 
-export function CheckboxTherms({ ontoggle }) {
+export function CheckboxTherms({ ontoggle, success }) {
   // AGB aus json => TODO: json: AGB
   const agb = "Es gibt noch keine AGB.";
 
@@ -14,15 +14,19 @@ export function CheckboxTherms({ ontoggle }) {
     setAcceptAGB(!acceptAGB);
     ontoggle(acceptAGB);
   };
-  console.log(acceptAGB);
+  // console.log(acceptAGB);
 
   return (
     <div className="flex items-center space-x-2">
-      <Checkbox id="terms" checked={acceptAGB} onClick={handleAcceptAGB} />
+      <Checkbox
+        id="terms"
+        checked={acceptAGB}
+        onClick={handleAcceptAGB}
+        disabled={success}
+      />
       <label
         htmlFor="terms"
-        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-      >
+        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
         <RegisterPopover title={"AGB akzeptieren"} textPopover={agb} />
       </label>
     </div>
