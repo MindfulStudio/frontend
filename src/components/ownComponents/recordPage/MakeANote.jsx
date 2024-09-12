81869a32070cbfeca498bc3edc075efde530ff02
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-09-12 15:33:03 +0200

    Component/makeanote (#111)
    * component/makeanote: added functionality; stored comment in state, infotext onclick, basic styling
    
    * component/makeanote: deleted accidental Button Import

--- src/components/ownComponents/recordPage/MakeANote.jsx
+++ src/components/ownComponents/recordPage/MakeANote.jsx
@@ -1,17 +1,41 @@
-import React from "react";
+import UserFeedbackText from "@/components/typo/UserFeedbackText";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
 
 const MakeANote = () => {
+  const [showNoteInfo, setShowNoteInfo] = useState(false);
+  const [comment, setComment] = useState("");
+
+  // show/hide UserFeedBackText
+  const handleInfo = () => {
+    setShowNoteInfo(!showNoteInfo);
+  };
+
+  const handleUserComment = (e) => {
+    setComment(e.target.value);
+    console.log(comment);
+  };
+
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center w-[290px] text-center">
       <p>Möchtest du noch etwas zu deinem Gefühl notieren?</p>
-      <div className="w-[290px] bg-white p-[22px] text-center mt-16 h-[423px] overflow-y-scroll">
-        {/* Adapt textarea width and height */}
-        <textarea
-          name="textarea"
-          id="textarea"
+      <div className="w-[290px] bg-white p-[22px] text-center mt-16 h-[423px]">
+        <Textarea
           placeholder="Hier ist Platz für deine Notiz."
-          autoFocus
-        ></textarea>
+          value={comment}
+          onChange={handleUserComment}
+        />
+        {showNoteInfo ? (
+          <UserFeedbackText
+            content="Im Journal kannst du deine Notiz jederzeit nachlesen. Du findest es in der Hauptnavigation."
+            type="info"
+            onClick={handleInfo} // Hide the text on click
+          />
+        ) : (
+          <button className=" underline mt-4 text-sm" onClick={handleInfo}>
+            Wo finde ich meine Notizen später wieder?
+          </button>
+        )}
       </div>
     </div>
   );
