4a3078a0d2c34e38837beb6368c6db8fded0705b
luisePkt <147039751+luisePkt@users.noreply.github.com>
2024-09-16 10:16:20 +0200

    Component/statistic one (#132)
    * setup: folder feelingsFamilies
    
    * adding feelingsFamily select-buttons
    
    * component/StatisticOne : adding new sub-components and implementing radialChart
    
    * component/StatisticOne : adding comments

--- src/components/usedDemoComponents/MetricsPageTabs.jsx
+++ src/components/usedDemoComponents/MetricsPageTabs.jsx
@@ -1,21 +1,10 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import StatisticOneTabsContent from "./StatisticOneTabsContent";
 import StatisticTwoTabsContent from "./StatisticTwoTabsContent";
 
 export function MetricsPageTabs() {
   return (
-    <Tabs defaultValue="account" className="w-[400px]">
+    <Tabs defaultValue="account" className="max-w-[350px]">
       <TabsList className="grid w-full grid-cols-2">
         <TabsTrigger value="account">Statistic One</TabsTrigger>
         <TabsTrigger value="password">Statistic Two</TabsTrigger>
