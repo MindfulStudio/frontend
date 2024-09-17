a116abeb03a646edc6bbdbe74218c054a2111428
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-09-17 17:04:08 +0200

    component/sleeprecord: fixed slider shadcn ui > extended tailwind utility classes for centralized slider styles; some refactoring in SleepRecord.jsx due to those ui changes and simplified enabling/disabling of the component

--- src/components/ownComponents/recordPage/SleepRecord.jsx
+++ src/components/ownComponents/recordPage/SleepRecord.jsx
@@ -2,40 +2,50 @@ import { Slider } from "@/components/ui/slider";
 import { useCheckinContext } from "@/utils/CheckinProvider";
 import { useEffect, useState } from "react";
 
-/////// Helper functions: ///////
+// Helper functions:
 
 // convert slider value (0-23) to German hours (17 Uhr - 16 Uhr)
 const sliderToHours = (value) => (value + 17) % 24;
 
 // Calculate sleep duration
 const calculateSleepDuration = (start, end) => {
+  // Local States:
   const startHour = sliderToHours(start);
   const endHour = sliderToHours(end);
   return endHour >= startHour ? endHour - startHour : 24 - startHour + endHour;
 };
 
-/////// Main component: ///////
-
 const SleepRecord = () => {
-  // States:
   const {
     sleepingHours,
     setSleepingHours,
     sleepingHoursRecorded,
     fetchSleepingHours,
   } = useCheckinContext();
-  const [range, setRange] = useState([6, 15]); // default: 23 to 9 (internally plus 17 hours)
+
+  const [range, setRange] = useState([6, 15]); // Default range: 23:00 - 16:00
 
   // Fetch and use today's sleeping hours if available
   useEffect(() => {
     fetchSleepingHours();
-  }, []);
+  }, [fetchSleepingHours]);
+
+  // Set the range based on recorded sleeping hours
+  useEffect(() => {
+    if (sleepingHoursRecorded) {
+      const startHour = (24 + 17 - sleepingHours) % 24;
+      const endHour = 17;
+      setRange([startHour - 17, endHour - 17]);
+    }
+  }, [sleepingHoursRecorded, sleepingHours]);
 
   //   Function handle slider change
   const handleSliderChange = (newRange) => {
-    setRange(newRange);
-    const sleepingRange = calculateSleepDuration(newRange[0], newRange[1]);
-    setSleepingHours(sleepingRange);
+    if (!sleepingHoursRecorded) {
+      setRange(newRange);
+      const sleepingRange = calculateSleepDuration(newRange[0], newRange[1]);
+      setSleepingHours(sleepingRange);
+    }
   };
 
   return (
@@ -45,19 +55,16 @@ const SleepRecord = () => {
           Von {sliderToHours(range[0])} Uhr bis {sliderToHours(range[1])} Uhr
         </p>
       )}
-      <p>
-        {!sleepingHoursRecorded && `(`}
-        {sleepingHours} Stunden{!sleepingHoursRecorded && `)`}
-      </p>
-      {!sleepingHoursRecorded && (
-        <Slider
-          min={0} // After converting: 17:00 Uhr
-          max={22} // After converting: 16:00 Uhr
-          step={1}
-          value={range}
-          onValueChange={handleSliderChange}
-        />
-      )}
+      <p className="p-2">{sleepingHours} Stunden</p>
+
+      <Slider
+        min={0} // After converting: 17:00 Uhr
+        max={22} // After converting: 16:00 Uhr
+        step={1}
+        value={range}
+        onValueChange={handleSliderChange}
+        disabled={sleepingHoursRecorded}
+      />
     </div>
   );
 };
