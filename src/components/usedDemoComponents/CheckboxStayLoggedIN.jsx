680522cb8d6ffba4afaa986e390f6c309017ba9d
Barış Balcı <126829019+barisbalcimusic@users.noreply.github.com>
2024-09-17 10:35:43 +0200

    feature/remember-me: remember-me functionality created (#151)

--- src/components/usedDemoComponents/CheckboxStayLoggedIN.jsx
+++ src/components/usedDemoComponents/CheckboxStayLoggedIN.jsx
@@ -1,27 +1,14 @@
 // "use client"
 
 import { Checkbox } from "@/components/ui/checkbox";
-import { useState } from "react";
 
-export function CheckboxStayLoggedIn({ ontoggle }) {
+export function CheckboxStayLoggedIn({ setStayLoggedIn }) {
   // AGB aus json => TODO: json: AGB
   const agb = "Es gibt noch keine AGB.";
 
-  const [stayLoggedIn, setStayLoggedIn] = useState(false);
-
-  const handleStayLoggedIn = () => {
-    setStayLoggedIn(!stayLoggedIn);
-    ontoggle(stayLoggedIn);
-  };
-  // console.log(stayLoggedIn);
-
   return (
     <div className="flex items-center space-x-2 pb-2">
-      <Checkbox
-        id="terms"
-        checked={stayLoggedIn}
-        onClick={handleStayLoggedIn}
-      />
+      <Checkbox id="terms" onClick={() => setStayLoggedIn((val) => !val)} />
       <p className="text-sm">eingeloggt bleiben</p>
       <label
         htmlFor="terms"
