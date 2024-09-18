b315c68ce440f8c9057759f9d99c86991d208790
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-09-18 10:00:19 +0200

    Component/sleeprecord: comment in slider.jsx (#173)
    * component/sleeprecord: fixed slider shadcn ui > extended tailwind utility classes for centralized slider styles; some refactoring in SleepRecord.jsx due to those ui changes and simplified enabling/disabling of the component
    
    * component/sleeprecord: added userfeedback for sleeprecord
    
    * component/sleeprecord: added comment in slider.jsx

--- src/components/ownComponents/recordPage/SleepActivityWeather.jsx
+++ src/components/ownComponents/recordPage/SleepActivityWeather.jsx
@@ -1,7 +1,5 @@
 //TODO: Request to Backend to check, if there are already any data for the current day => getCheckInsFromToday
 
-//TODO: Check later, when the Recordprocess is connected to the Backend: It also should be possible, that the user can skip this part of the check in or skip selected parts of it (e.g. only check in the weather, but not the sleep time; only change the sleep time, but not the acitvity, etc.) > Does every section need an own skip button? Or is it enough to allow the skip for this whole part of the recordprocess?
-
 // import subcomponents
 import { WeatherToggles } from "@/components/usedDemoComponents/WeatherToggles";
 import { ActivitySwitch } from "@/components/usedDemoComponents/ActivitySwitch";
