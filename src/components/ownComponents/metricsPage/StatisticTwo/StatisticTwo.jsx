ea5aa0f4c6bb7b9f4069da1160ebff5f4d7b23df
luisePkt <madame.moon@gmx.de>
2024-09-21 15:23:35 +0200

    component/StatisticTwo : implemnting and adjusting move back in statisticOne and statisticTwo

--- src/components/ownComponents/metricsPage/StatisticTwo/StatisticTwo.jsx
+++ src/components/ownComponents/metricsPage/StatisticTwo/StatisticTwo.jsx
@@ -1,24 +1,27 @@
 import { useMetricsContext } from "@/utils/MetricsProvider";
 import { useEffect } from "react";
 
-
 const StatisticTwo = () => {
-    const {
-        showMetricsTwo,
-        selectedTag,
-        setSelectTag,
-        statisticsByTag,
-        disableTag,
-        setDisableTag,
-        setMetricsTwoStatus,
-        fetchStatsByTag,
-      } = useMetricsContext();
+  const {
+    showMetricsTwo,
+    selectedTag,
+    setSelectTag,
+    statisticsByTag,
+    disableTag,
+    setDisableTag,
+    setMetricsTwoStatus,
+    fetchStatsByTag,
+  } = useMetricsContext();
 
-    useEffect(() => {
-        fetchStatsByTag("morgens");
-        // TODO: insert a state variable from MetricsProvider here as a parameter (instead of "morgens")
-      }, []);
-  return <div>StatisticTwo</div>;
+  useEffect(() => {
+    fetchStatsByTag("morgens");
+    // TODO: insert a state variable from MetricsProvider here as a parameter (instead of "morgens")
+  }, []);
+  return (
+    <div>
+      {selectedTag.name}
+    </div>
+  );
 };
 
 export default StatisticTwo;
