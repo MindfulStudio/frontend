d1d2f3d778429617eef9e80341d430c0b7e4ae41
hannahnier <hannahr@posteo.de>
2024-09-25 15:21:29 +0200

    refactoring/routers: move routers to new folder

---
+++ src/utils/routers/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useAuthContext } from "../AuthProvider";
+
+const PrivateRoute = ({ children }) => {
+  const { isLoggedIn } = useAuthContext();
+
+  // navigate to home page if user is not logged in
+  return isLoggedIn ? children : <Navigate to="/" />;
+};
+
+export default PrivateRoute;
