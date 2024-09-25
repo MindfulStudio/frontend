e45f804b0e903628aacc0ecba3be319d27eaaaca
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-09-25 12:17:12 +0200

    Page/recordpage/cleanup (#221)
    * page/recordpage/cleanup: deleted comments, improved comments, reviewed code
    
    * page/recordpage/cleanup: comments in dashboardpage
    
    * page/recordpage/cleanup: Comments in BadgeInfoPopup
    
    * page/recordpage/cleanup: CheckInFeelingDisplay.jsx; improved or deleted comments
    
    * page/recordpage/cleanup: comment structure in CheckInFeelingDisplay.jsx
    
    * page/recordpage/cleanup: added comment in CheckInFeelingDisplay.jsx
    
    * page/recordpage/cleanup: Comments deleted or improved in Feelingsselector.jsx, code reviewed
    
    * page/recordpage/cleanup: code in InpurAndButtonForCustomTag reviewed, comment structure improved
    
    * page/recordpage/cleanup: cleanup, codereview and comments in MakeANote.jsx
    
    * page/recordpage/cleanup: code review and comments in SleepActivityWeather.jsx
    
    * page/recordpage/cleanup: removed typo component import in MakeANote.jsx
    
    * page/recordpage/cleanup: code review und comments in SleepRecord.jsx
    
    * page/recordpage/cleanup: removed changes from errorpage due to separation of concerns

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -10,15 +10,16 @@ import GreetingComponent from "@/components/ownComponents/dashboardPage/Greeting
 import UserFeedbackText from "@/components/typo/UserFeedbackText";
 
 const DashboardPage = () => {
+  // ------------------------------- States from Contexts -----------------------
   const { fetchAllCustoms } = useUserContext();
   const { setCheckinUserFeedback, checkinUserFeedback } = useCheckinContext(); // checkinUserFeedback depends on the success of a previous checkin
 
-  // fetch customs from backend when the component is mounted:
+  // ------------------------------- Fetching customs ---------------------------
   useEffect(() => {
     fetchAllCustoms();
   }, [fetchAllCustoms]);
 
-  // Timer to clear userfeedback after a few seconds
+  // -------------------------  Timer to clear userfeedback ---------------------
   useEffect(() => {
     if (checkinUserFeedback.message) {
       const timer = setTimeout(
@@ -29,8 +30,10 @@ const DashboardPage = () => {
     }
   }, [checkinUserFeedback]);
 
+  // ------------------------------- Render -------------------------------------
   return (
     <div className="flex flex-col justify-center">
+      {/* User feedback */}
       {checkinUserFeedback.message && (
         <div className="fixed inset-0 flex justify-center items-start z-50 mt-5 pointer-events-none">
           <UserFeedbackText
@@ -41,6 +44,7 @@ const DashboardPage = () => {
         </div>
       )}
 
+      {/* Main content */}
       <main className="flex-grow pt-[109px] px-[50px] flex flex-col items-center">
         <GreetingComponent />
         <FeelingsFamilyComponent />
