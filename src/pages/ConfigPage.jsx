4573948b2ddec5884576aa57e3d99d1c0576b131
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-10-07 10:51:04 +0200

    fix/responsive-workaround-desktop: added desktop responsive workaround to ConfigPage

--- src/pages/ConfigPage.jsx
+++ src/pages/ConfigPage.jsx
@@ -3,7 +3,7 @@ import { ConfigTabs } from "@/components/usedDemoComponents/ConfigTabs";
 const ConfigPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-slate-900">
-      <main className="flex flex-col items-center w-[430px] h-screen pt-[109px]bg-background">
+      <main className=" pt-28 flex-grow flex flex-col items-center w-[430px] h-screen bg-background">
         <ConfigTabs />
       </main>
     </div>
