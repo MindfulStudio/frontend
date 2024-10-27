a4b4b820f98e23c84f630115c159a12fa562d6c7
hannahnier <hannahr@posteo.de>
2024-10-27 12:36:48 +0100

    fix/MetricsProvider: remove debugging logs

--- src/utils/services/getStatisticsByFamily.js
+++ src/utils/services/getStatisticsByFamily.js
@@ -2,7 +2,6 @@ export const getStatisticsByFamily = async (
   setStatisticsByFamily,
   selectedFeelingsFamily
 ) => {
-  console.log("fetching statistics by family");
   try {
     const baseURL = import.meta.env.VITE_baseURL;
     const basePathThree = import.meta.env.VITE_basePathThree;
